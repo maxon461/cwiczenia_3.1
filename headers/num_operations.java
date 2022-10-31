858f00bff9abf2cb482c3bf884033e2c4804a587
Maxon laptop <vasmoto00@gmail.com>
2022-10-31 22:44:39 +0100

    done

--- headers/num_operations.java
+++ headers/num_operations.java
@@ -1,33 +1,44 @@
 package headers;
+
 import java.util.*;
+
 public class num_operations {
-    public static void max(int [] arr){
+    public static void max(int[] arr) {
         int res = -100;
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]>res) {res = arr[i];}
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > res) {
+                res = arr[i];
+            }
         }
-        System.out.println("max is: "+res);
+        System.out.println("max is: " + res);
     }
-    public static void min(int [] arr){
+
+    public static void min(int[] arr) {
         int res = 100;
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]<res) {res = arr[i];}
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < res) {
+                res = arr[i];
+            }
         }
-        System.out.println("min is: "+res);
+        System.out.println("min is: " + res);
     }
-    public static void average(int [] arr){
+
+    public static void average(int[] arr) {
         double res = 0;
-        for (int i=0;i<arr.length;i++){
-            res+=arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            res += arr[i];
         }
         res /= arr.length;
-        System.out.println("average is: "+res);
+        System.out.println("average is: " + res);
     }
-    public static ArrayList<Integer> even_num(int [] arr){
+
+    public static ArrayList<Integer> even_num(int[] arr) {
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i=0;i<arr.length;i++){
-            if(arr[i] % 2 == 0 ) {list.add(arr[i]);}
-            
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                list.add(arr[i]);
+            }
+
         }
         return list;
     }
--- main/App.java
+++ main/App.java
@@ -1,15 +1,18 @@
 package main;
+
 import java.util.*;
 import headers.*;
+
 public class App {
-    public static void fill_arr(int [] arr ){
+    public static void fill_arr(int[] arr) {
         Random rand = new Random();
-        for(int i = 0; i<arr.length;i++){
-            arr[i] = (rand.nextInt()%100);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (rand.nextInt() % 100);
         }
     }
+
     public static void main(String[] args) {
-        int [] arr = new int[100];
+        int[] arr = new int[100];
         fill_arr(arr);
         displays.display_arr(arr);
         num_operations.max(arr);
@@ -18,18 +21,6 @@ public class App {
         ArrayList<Integer> list = new ArrayList<>();
         list = num_operations.even_num(arr);
         displays.display_list(list);
-        
-        
-
-
-
-        
-
-        
-
-        
-        
-       
 
     }
 }
